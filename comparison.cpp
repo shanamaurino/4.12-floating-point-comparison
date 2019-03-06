e81cd9f85c22e3ddb91d161547348c744fe143c9
shanamaurino <33281843+shanamaurino@users.noreply.github.com>
2019-03-06 17:31:31 -0500

    Add files via upload

---
+++ comparison.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include <cmath>
+
+using namespace std;
+
+
+
+int main() {
+
+   double targetValue = 0.3333;
+
+   double sensorReading = 0.0;
+
+
+
+   sensorReading = 1.0/3.0;
+
+
+
+   if (fabs(targetValue - sensorReading) < 0.0001) {
+
+    cout << "Equal" << endl;
+
+   }
+
+   else {
+
+      cout << "Not equal" << endl;
+
+   }
+
+
+
+   return 0;
+}
